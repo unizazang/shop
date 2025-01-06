2b92a6a1ee64b20fc54b99bcde02792b2eb413d1
unizazang <zifnffk32123@gmail.com>
2025-01-06 23:45:59 +0900

    1

--- src/Detail.js
+++ src/Detail.js
@@ -1,14 +1,43 @@
+// import { useParams } from "react-router-dom";
+
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
+
+let YellowBtn = styled.button`
+  background: yellow;
+  color: black;
+  padding: 10px;
+`;
+
+/* ========== Styled component 사용법 ==============================
+  let YellowBtn = styled.button`
+    background: yellow;
+    color: black;
+    padding: 10px;
+  ` //백틱으로 써서 그 안에 스타일을 집어넣음
+
+  이걸 변수에 저장해서 내가 원하는데에 갖다 쓸 수 있다.
+  스타일이 입혀진 하나의 컴포넌트를 생성하는 문법이기 때문에 대문자로 작명해야 한다
+
+
+*/
 
 function Detail(props) {
-  let { id } = useParams(); //유저가 URL 파라미터에 입력한 거 가져올 수 있는 훅
+  // let { id } = useParams(); //유저가 URL 파라미터에 입력한 거 가져올 수 있는 훅
+
+  let { id } = useParams();
+  let foundProduct = props.shoes.find((product) => {
+    return product.id === parseInt(id);
+  });
+  console.log(foundProduct);
 
   /* 오늘의 응용 =====================
     /detail/0 접속시 0번째 상품이 아니라(정렬시 이상해짐)
     상품id가 0인 걸 보여주면 좋을 듯.
   */
-  return (
+  return foundProduct ? (
     <div className="container">
+      <YellowBtn>버튼</YellowBtn>
       <div className="row">
         <div className="col-md-6">
           <img
@@ -17,13 +46,15 @@ function Detail(props) {
           />
         </div>
         <div className="col-md-6">
-          <h4 className="pt-5">{props.shoes[id].title}</h4>
-          <p>{props.shoes[id].content}</p>
-          <p>{props.shoes[id].price}원</p>
+          <h4 className="pt-5">{foundProduct.title}</h4>
+          <p>{foundProduct.content}</p>
+          <p>{foundProduct.price}원</p>
           <button className="btn btn-danger">주문하기</button>
         </div>
       </div>
     </div>
+  ) : (
+    <div>상품을 찾을 수 없ㅅ브니다.</div>
   );
 }
 
