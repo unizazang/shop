01f7131e8086b07f12e2b27c65f820fcf866eb4a
unijajang <zifnffk32123@gmail.com>
2025-01-19 02:23:34 +0900

    redux store

--- src/routes/Cart.js
+++ src/routes/Cart.js
@@ -1,26 +1,58 @@
 import {Table} from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Cart(){
+
+  // redux store에 보관한 변수 꺼내쓰기 -----------------------
+
+ let state = useSelector((state)=>{return state.stock}) // 가져와주는 함수
+    // 이 안에는 store 에 있던 모든 state가 들어있다
+    console.log(state[1]);
+
+    /* --------------------- useSelector 사용 팁 ----------------------- 
+    useSelector((state)=>{return state}) <- 여기에는 store 안에있는 모든 state를 뜻하기 때문에
+    어떤 state를 가져오고 싶은지 여기에 써줄수 있다!!
+    let userState = useSelector((state)=>{return state.user})
+    이런 식으로 딱 가져오고 싶은 state만 따로 변수에 담을 수가 있음.
+    
+    당연하지만 중괄호 return 생략해도 됨
+    useSelector(state => state.stock) 
+    
+    */
+
+    let cart = useSelector((state) => state.cart)
+
+    
     return (
         <div>
            <Table>
-  <thead>
-    <tr> 
-      <th>#</th>
-      <th>상품명</th>
-      <th>수량</th>
-      <th>변경하기</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>1</td>
-      <td>안녕</td>
-      <td>안녕</td>
-      <td>안녕</td>
-    </tr>
-  </tbody>
-</Table>  
+              {
+                cart.map((_, i)=>{
+                  return (
+                    <tr> 
+                      <th>{cart[i].id}</th>
+                      <th>{cart[i].name}</th>
+                      <th>{cart[i].count}</th>
+                      <th>변경하기</th>
+                    </tr>
+                  )
+                })
+              }
+                {/* <tr> 
+                  <th>{cart[0].id}</th>
+                  <th>{cart[0].name}</th>
+                  <th>{cart[0].count}</th>
+                  <th>변경하기</th>
+                </tr>
+
+                <tr>
+                  <td>{cart[1].id}</td>
+                  <td>{cart[1].name}</td>
+                  <td>{cart[1].count}</td>
+                  <td>변경하기</td>
+                </tr> */}
+
+            </Table>  
         </div>
     )
 }
--- src/store.js
+++ src/store.js
@@ -4,8 +4,41 @@
 // 2. index.js 가서 <Provider store={store}> 라는 걸로 App 감싸기
 // -> 그럼 App 과 그 자식 컴포넌트들은 다 store.js에 있는 state 사용 가능.
 
-import { configureStore } from '@reduxjs/toolkit'
+// 근데 store에다가 모든 state를 보관할 필요는 없다!!!!! 공유 안하는 거는 필요한 데만 useState()로 만들어서 쓰면 됨...
+
+
+import { configureStore , createSlice } from '@reduxjs/toolkit'
+
+
+// state 만드는 법 - 이거 state 하나를 slice 라고 부른다
+// createSlice()는 useState()랑 비슷한 용도
+let user = createSlice({
+  name : 'user' , // state 이름
+  initialState : 'kim' //state 값
+});  
+// 이 안에 객체 로 넣어야함!!!
+
+let stock = createSlice({
+  name : 'stock',
+  initialState : [10, 11, 12]
+})
+
+let cart = createSlice({
+  name : 'cart',
+  initialState : [
+    {id : 0, name : 'White and Black', count : 2},
+    {id : 2, name : 'Grey Yordan', count : 1}
+  ] 
+  // 초기값을 여기 넣는 거임
+
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: {
+  // state를 만들었다고 끝이아니고 이 안에 ★★등록★★해야 사용이 가능!!!
+    user : user.reducer,
+    stock : stock.reducer,
+    cart : cart.reducer,
+   }
 }) 
+// 객체로 들어있으니까 안에서 콤마로 구분
